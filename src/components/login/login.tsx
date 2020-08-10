1999fbf04829b1fda899f73130d96530f0db1acf
Andre Marques <utilizador@MBPdeUtilizador.Home>
2020-08-10 16:37:01 +0100

    Added routing and forms

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -4,10 +4,27 @@ import logo from '../../assets/images/logo.png';
 import facebook from '../../assets/images/facebook.svg';
 import google from '../../assets/images/google.svg';
 import linkedin from '../../assets/images/linkedin.svg';
+import { connect } from 'react-redux';
+import { setUserAction } from '../../shared/store/actions/user.actions';
+import { Dispatch } from 'redux';
+import { RouteComponentProps } from 'react-router-dom';
 
-class Login extends React.Component {
+interface Props extends RouteComponentProps {
+    setUserType(username: string): void
+};
+
+class Login extends React.Component<Props, {}> {
     state = {
-        login: true
+        login: true,
+        signUpForm: {
+            name: "",
+            email: "",
+            password: ""
+        },
+        signInForm: {
+            email: "",
+            password: ""
+        }
     };
     render() {
         return (
@@ -39,24 +56,109 @@ class Login extends React.Component {
                 <div className={`login__create-container ${this.state.login ? 'login__create-container--active' : 'login__create-container--inactive'}`}>
                     Create Account
                     <div className="login__create-container__social-container">
-                        <img className="login__create-container__social-container--facebook-icon" src={facebook} alt=""/>
-                        <img className="login__create-container__social-container--google-icon" src={google} alt=""/>
-                        <img className="login__create-container__social-container--linkedin-icon" src={linkedin} alt=""/>
+                        <img className="login__create-container__social-container--facebook-icon" src={facebook} alt="" />
+                        <img className="login__create-container__social-container--google-icon" src={google} alt="" />
+                        <img className="login__create-container__social-container--linkedin-icon" src={linkedin} alt="" />
                     </div>
                     <span className="login__create-container--info-text">or use email for your registration</span>
                     <div className="login__create-container__form-container">
-                        <form onSubmit={() => alert('Submitted')}>
-
+                        <form className="login__create-container__form-container__form" onSubmit={(e) => {
+                            e.preventDefault();
+                            this.signUp();
+                        }}>
+                            <input
+                                className="login__create-container__form-container__form--name"
+                                type="text"
+                                placeholder="Name"
+                                value={this.state.signUpForm.name}
+                                onChange={(value) => this.setState({
+                                    signUpForm: {
+                                        name: value.target.value,
+                                        email: this.state.signUpForm.email,
+                                        password: this.state.signUpForm.password
+                                    }
+                                })}
+                                required />
+                            <input
+                                className="login__create-container__form-container__form--email"
+                                type="email"
+                                placeholder="Email"
+                                value={this.state.signUpForm.email}
+                                onChange={(value) => this.setState({
+                                    signUpForm: {
+                                        email: value.target.value,
+                                        name: this.state.signUpForm.name,
+                                        password: this.state.signUpForm.password
+                                    }
+                                })}
+                                required />
+                            <input
+                                className="login__create-container__form-container__form--password"
+                                type="password"
+                                placeholder="Password"
+                                value={this.state.signUpForm.password}
+                                onChange={(value) => this.setState({
+                                    signUpForm: {
+                                        password: value.target.value,
+                                        name: this.state.signUpForm.name,
+                                        email: this.state.signUpForm.email
+                                    }
+                                })}
+                                required />
+                            <button
+                                className="login__create-container__form-container__form--submit">
+                                Sign Up
+                            </button>
                         </form>
                     </div>
                 </div>
-                <div className={`login__register-container ${!this.state.login ? 'login__register-container--active' : 'login__register-container--inactive'}`}>
-                    <div className="login__register-container__logo-container">
-                        <img className="login__register-container__logo-container--image" src={logo} alt="Budwriter" />
+                <div className={`login__login-container ${!this.state.login ? 'login__login-container--active' : 'login__login-container--inactive'}`}>
+                    <div className="login__login-container__logo-container">
+                        <img className="login__login-container__logo-container--image" src={logo} alt="Budwriter" />
                         Budwriter
                     </div>
-                    <div className="login__register-container__main-container">
-                        Sign in to Budwriter
+                    <div className="login__login-container__main-container">
+                        <div className="login__login-container__main-container__social-container">
+                            <img className="login__login-container__main-container__social-container--facebook-icon" src={facebook} alt="" />
+                            <img className="login__login-container__main-container__social-container--google-icon" src={google} alt="" />
+                            <img className="login__login-container__main-container__social-container--linkedin-icon" src={linkedin} alt="" />
+                        </div>
+                        <span className="login__login-container__main-container--info-text">or use email for your login</span>
+                        <div className="login__login-container__main-container__form-container">
+                            <form className="login__login-container__main-container__form-container__form" onSubmit={(e) => {
+                                e.preventDefault();
+                                this.signIn();
+                            }}>
+                                <input
+                                    className="login__login-container__main-container__form-container__form--email"
+                                    type="email"
+                                    placeholder="Email"
+                                    value={this.state.signInForm.email}
+                                    onChange={(value) => this.setState({
+                                        signInForm: {
+                                            email: value.target.value,
+                                            password: this.state.signInForm.password
+                                        }
+                                    })}
+                                    required />
+                                <input
+                                    className="login__login-container__main-container__form-container__form--password"
+                                    type="password"
+                                    placeholder="Password"
+                                    value={this.state.signInForm.password}
+                                    onChange={(value) => this.setState({
+                                        signInForm: {
+                                            password: value.target.value,
+                                            email: this.state.signInForm.email
+                                        }
+                                    })}
+                                    required />
+                                <button
+                                    className="login__login-container__main-container__form-container__form--submit">
+                                    Sign In
+                            </button>
+                            </form>
+                        </div>
                     </div>
                 </div>
                 <div className={`login__hello-container ${!this.state.login ? 'login__hello-container--active' : 'login__hello-container--inactive'}`}>
@@ -79,6 +181,36 @@ class Login extends React.Component {
             </div>
         );
     }
+
+    signUp() {
+        this.setState({
+            signUpForm: {
+                name: "",
+                password: "",
+                email: ""
+            }
+        });
+    }
+
+    signIn() {
+        this.props.history.push("/dashboard");
+        this.props.setUserType(this.state.signInForm.email);
+        this.setState({
+            signInForm: {
+                password: "",
+                email: ""
+            }
+        });
+    }
 }
 
-export default Login;
+function mapDispatchToProps(dispatch: Dispatch) {
+    return {
+        setUserType: (username: string) =>
+          dispatch(setUserAction({
+              username
+          }))
+    };
+}
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/shared/store/reducers/user.reducers.ts
+++ src/shared/store/reducers/user.reducers.ts
@@ -1,14 +1,15 @@
 import { User } from "../../models/user.models";
 import { Action } from "redux";
-import { setUserAction } from "../actions/user.actions";
+import { setUserType } from "../actions/user.actions";
 
 export const userInitialState: User = {
     username: ''
 }
 
 export const userState = (state: User = userInitialState, action: Action | any): User => {
+    console.log(action.type);
     switch (action.type) {
-        case setUserAction:
+        case setUserType:
             return {
                 ...state,
                 username: action.payload.username
