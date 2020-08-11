69cba355a40ef9be045e954482aae676ab0f09c2
Andre Marques <utilizador@MBPdeUtilizador.Home>
2020-08-11 09:59:58 +0100

    Added custom fonts + dashboard success page

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -1,13 +1,44 @@
 import React from 'react';
+import './dashboard.scss';
+import { RouteComponentProps } from 'react-router-dom';
+import { State } from '../../shared/models/state.models';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+import { setUserAction } from '../../shared/store/actions/user.actions';
 
-class Dashboard extends React.Component {
+interface Props extends RouteComponentProps {
+    username: string;
+    setUserType(username: string): void
+}
+
+class Dashboard extends React.Component<Props> {
     render() {
         return (
-            <div>
-                Heyyy
+            <div className="dashboard">
+                Welcome, {this.props.username}
+                <button onClick={this.signOut.bind(this)} className="dashboard--button">Sign out</button>
             </div>
-        )
+        );
+    }
+
+    signOut() {
+        this.props.setUserType('');
+        this.props.history.push('/');
     }
 }
 
-export default Dashboard;
+function mapStateToProps(state: State) {
+    const { userState } = state;
+    return { username: userState.username };
+}
+
+function mapDispatchToProps(dispatch: Dispatch) {
+    return {
+        setUserType: (username: string) =>
+          dispatch(setUserAction({
+              username
+          }))
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
