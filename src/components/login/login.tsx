a4bf2c5ec096b2f86496f5e7da126306afbdcc74
Andre Marques <utilizador@MBPdeUtilizador.Home>
2020-08-14 14:55:26 +0100

    Added mobile support

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -26,6 +26,7 @@ class Login extends React.Component<Props> {
             password: ""
         }
     };
+
     render() {
         return (
             <div className="login">
--- src/shared/store/reducers/user.reducers.ts
+++ src/shared/store/reducers/user.reducers.ts
@@ -2,11 +2,17 @@ import { User } from "../../models/user.models";
 import { Action } from "redux";
 import { setUserType } from "../actions/user.actions";
 
+interface UserActions extends Action {
+    payload: {
+        username: string;
+    }
+}
+
 export const userInitialState: User = {
     username: ''
 }
 
-export const userState = (state: User = userInitialState, action: Action | any): User => {
+export const userState = (state: User = userInitialState, action: UserActions): User => {
     switch (action.type) {
         case setUserType:
             return {
